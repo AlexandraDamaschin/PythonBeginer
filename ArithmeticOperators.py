01a27f3bb62df9b6bcf0b151ebd8cb710b2d2900
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-20 20:19:13 +0100

    variables name need to contain _

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -1,6 +1,11 @@
 # sum numbers
-print((23+32+64)/3)
+print((23 + 32 + 64) / 3)
 
 # order is important
-print((17*6)-((9*7)+(5*7)))
+print((17 * 6) - ((9 * 7) + (5 * 7)))
 
+# variables name need to contain _
+mv_population = 12345
+mv_area = 11.23
+mv_density = mv_population / mv_area
+print(mv_density)
