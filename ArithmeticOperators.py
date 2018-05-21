980388cbd8f7f89c358b79fa88b52915ab08e33d
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-21 21:39:24 +0100

    number of solved items

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -1,28 +1,28 @@
-# sum numbers
+# 1. sum numbers
 print((23 + 32 + 64) / 3)
 
-# order is important
+# 2.order is important
 print((17 * 6) - ((9 * 7) + (5 * 7)))
 
-# variables name need to contain _
+# 3.variables name need to contain _
 mv_population = 12345
 mv_area = 11.23
 mv_density = mv_population / mv_area
 print(mv_density)
 
-# decrease by 10%
+# 4.decrease by 10%
 mv_population *= 0.9
 
-# decrease by 5%
+# 5.decrease by 5%
 mv_population *= 0.95
 
-# print type of inputted value
+# 6.print type of inputted value
 print(type(4.0))
 
-# convert int to float
+# 7.convert int to float
 print(int(2.3))
 
-# boolean example
+# 8.boolean example
 sf_population, sf_area = 864816, 231.89
 rio_population, rio_area = 6453682, 486.5
 
@@ -32,25 +32,25 @@ rio_de_janeiro_pop_density = rio_population / rio_area
 # Write code that prints True if San Francisco is denser than Rio, and False otherwise
 print(san_francisco_pop_density > rio_de_janeiro_pop_density)
 
-# strings
+# 9.strings
 welcome = 'Hello world'
 print(welcome)
 
-# length of a string
+# 10.length of a string
 welcome_length = len(welcome)
 print(welcome_length)
 
-# index into strings
+# 11.index into strings
 print(welcome[1])
 
-# write a sentence
+# 12.write a sentence
 username = "Kinari"
 timestamp = "04:50"
 url = "http://petshop.com/pets/mammals/cats"
 
 print(username + " accessed the site " + url + " at " + timestamp + ".")
 
-# calculate length of a sentence
+# 13.calculate length of a sentence
 given_name = "William"
 middle_names = "Bradley"
 family_name = "Pitt"
@@ -62,3 +62,5 @@ name_length = len(full_name)
 # Now we check to make sure that the name fits within the driving license character limit
 driving_license_character_limit = 28
 print(name_length <= driving_license_character_limit)
+
+# 14.
