603dc8f87ab65ffff7007b815077aa6beb244a06
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-21 21:38:02 +0100

    lenght of full name

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -49,3 +49,16 @@ timestamp = "04:50"
 url = "http://petshop.com/pets/mammals/cats"
 
 print(username + " accessed the site " + url + " at " + timestamp + ".")
+
+# calculate length of a sentence
+given_name = "William"
+middle_names = "Bradley"
+family_name = "Pitt"
+
+full_name = (given_name + " " + middle_names + " " + family_name)
+
+name_length = len(full_name)
+
+# Now we check to make sure that the name fits within the driving license character limit
+driving_license_character_limit = 28
+print(name_length <= driving_license_character_limit)
