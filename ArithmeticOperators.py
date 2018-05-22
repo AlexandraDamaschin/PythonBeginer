374cbbcccb52b08bcc42a447efe350b0da02b068
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-22 21:07:16 +0100

    Print last 3 from a list

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -116,3 +116,11 @@ days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 num_days = days_in_month[7]
 
 print(num_days)
+
+# 21. Print last 3
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+
+print(eclipse_dates[-3:])
