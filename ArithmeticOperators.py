9404620497c33d83512a65ee481890df694e69b9
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-22 19:47:34 +0100

    Sales example

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -63,4 +63,17 @@ name_length = len(full_name)
 driving_license_character_limit = 28
 print(name_length <= driving_license_character_limit)
 
-# 14.
+# 14.Sales example
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+mon_sales = int(mon_sales)
+tues_sales = int(tues_sales)
+wed_sales = int(wed_sales)
+thurs_sales = int(thurs_sales)
+fri_sales = int(fri_sales)
+total_sales = (mon_sales + tues_sales + wed_sales + thurs_sales + fri_sales)
+print("This week's total sales: " + str(total_sales))
