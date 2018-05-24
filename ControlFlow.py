9db5d2bd3bb9fdc94a15eada82ea70fe1c82ed65
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-24 19:18:29 +0100

    Bus ride cost

--- ControlFlow.py
+++ ControlFlow.py
@@ -27,3 +27,24 @@ if number % 2 == 0:
     print("Number " + str(number) + " is even.")
 else:
     print("Number " + str(number) + " is odd.")
+
+# 4. Bus ride cost
+age = 12
+
+free_up_to_age = 4
+child_up_to_age = 18
+senior_from_age = 65
+# These lines set the bus fares
+concession_ticket = 1.25
+adult_ticket = 2.50
+# Here is the ticket price logic
+if age <= free_up_to_age:
+    ticket_price = 0
+elif age <= child_up_to_age:
+    ticket_price = concession_ticket
+elif age >= senior_from_age:
+    ticket_price = concession_ticket
+else:
+    ticket_price = adult_ticket
+message = "Somebody who is {} years old will pay ${} to ride the bus.".format(age, ticket_price)
+print(message)
