5d86b808a191bf1e4af0e9631c2ca9c3feb93d19
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-24 19:13:04 +0100

    IF ELIF ELSE

--- ControlFlow.py
+++ ControlFlow.py
@@ -7,3 +7,16 @@ if phone_balance < 5:
     bank_balance -= 10
 
 print(phone_balance, bank_balance)
+
+# 2. IF ELIF ELSE
+season = "summer"
+if season == 'spring':
+    print('plant the garden!')
+elif season == 'summer':
+    print('water the garden!')
+elif season == 'fall':
+    print('harvest the garden!')
+elif season == 'winter':
+    print('stay indoors!')
+else:
+    print('unrecognized season')
