2ab057eae20889748a28378dfb4e672bc90a1354
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-24 18:42:16 +0100

    Add elements in dictionary

--- ArithmeticOperators.py
+++ ArithmeticOperators.py
@@ -178,3 +178,14 @@ helium = elements["helium"]  # get the helium dictionary
 hydrogen_weight = elements["hydrogen"]["weight"]  # get hydrogen's weight
 print(helium)
 print(hydrogen_weight)
+
+# 29. Add elements in dictionary
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+
+elements['hydrogen']['is_noble_gas'] = False
+elements['helium']['is_noble_gas'] = True
+
+print(elements['hydrogen']['is_noble_gas'])
+
+print(elements['helium']['is_noble_gas'])
