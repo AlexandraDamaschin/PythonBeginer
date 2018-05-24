bc7d5af3cb035b8b34dcf38d5479cb0fc3522717
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-24 19:36:38 +0100

    States taxes

--- ControlFlow.py
+++ ControlFlow.py
@@ -78,3 +78,24 @@ elif guess == answer:
     result = "Nice!  Your guess matched the answer!"
 
 print(result)
+
+# 7. States taxes
+state = "CA"
+purchase_amount = 100
+
+if state == "CA":
+    tax_amount = .075
+    total_cost = purchase_amount * (1 + tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == "MN":
+    tax_amount = .095
+    total_cost = purchase_amount * (1 + tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == "NY":
+    tax_amount = .089
+    total_cost = purchase_amount * (1 + tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
