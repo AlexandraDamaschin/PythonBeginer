67e78a97975c2440ff14f82394a23316891e9c76
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-26 10:28:55 +0100

    Air traffic control program

--- ControlFlow.py
+++ ControlFlow.py
@@ -98,3 +98,13 @@ elif state == "NY":
     result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
 
 print(result)
+
+# 8. Air traffic control program
+altitude = 10000
+speed = 250
+propulsion = "Propeller"
+
+print(altitude < 1000 and speed > 100)
+print((propulsion == "Jet" or propulsion == "Turboprop") and speed < 300 and altitude > 20000)
+print(not (speed > 400 and propulsion == "Propeller"))
+print((altitude > 500 and speed > 100) or not propulsion == "Propeller")
