6eabab0b178c5ee00fc85e0da11ab42cafc08252
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-26 11:31:06 +0100

    Fruit or not fruit

--- ControlFlow.py
+++ ControlFlow.py
@@ -201,3 +201,16 @@ for fruit, count in basket_items.items():
         result += count
 
 print("There are {} fruits in the basket.".format(result))
+
+# 19. Fruit or not fruit
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for fruit, count in basket_items.items():
+    if fruit in fruits:
+        fruit_count += count
+    else:
+        not_fruit_count += count
+
+print("The number of fruits is {}.  There are {} items that are not fruits.".format(fruit_count, not_fruit_count))
