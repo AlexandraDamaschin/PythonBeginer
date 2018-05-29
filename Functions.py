a2a0ca8bf2a70f2c8b9e8bcc57c68aa4d5df3fa9
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-29 20:34:55 +0100

    Lambda Expressions

--- Functions.py
+++ Functions.py
@@ -67,4 +67,34 @@ def readable_timedelta(days):
 # test your function
 print(readable_timedelta(10))
 
-# 5.
+# 5.Lambda Expressions
+multiply = lambda x, y: x * y
+print(multiply(4, 7))
+
+# 6.  Lambda with Map
+numbers = [
+    [34, 63, 88, 71, 29],
+    [90, 78, 51, 27, 45],
+    [63, 37, 85, 46, 22],
+    [51, 22, 34, 11, 18]
+]
+
+# function
+# def mean(num_list):
+#    return sum(num_list) / len(num_list)
+# lambda expresion 1
+# mean= lambda num_list: sum(num_list)/len(num_list)
+# lambda expresion 2
+# averages = list(map(mean, numbers))
+averages = list(map(lambda x: sum(x) / len(x), numbers))
+print(averages)
+
+# 7.Lambda with filter
+cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+# funtionc
+# def is_short(name):
+#   return len(name) < 10
+
+# short_cities = list(filter(is_short, cities))
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)
