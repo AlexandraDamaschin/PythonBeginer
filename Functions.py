84cdfe77bc3bb97744cabb3b05f18bd69afca40b
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-29 18:44:18 +0100

    Print vs. Return in Functions

--- Functions.py
+++ Functions.py
@@ -5,3 +5,23 @@ def cylinder_volume(height, radius):
 
 
 print(cylinder_volume(2, 3))
+
+
+# 2. Print vs. Return in Functions
+# this prints something, but does not return anything
+def show_plus_ten(num):
+    print(num + 10)
+
+
+# this returns something
+def add_ten(num):
+    return (num + 10)
+
+
+print('Calling show_plus_ten...')
+return_value_1 = show_plus_ten(5)
+print('This function returned: {}'.format(return_value_1))
+
+print('\nCalling add_ten...')
+return_value_2 = add_ten(5)
+print('This function returned: {}'.format(return_value_2))
