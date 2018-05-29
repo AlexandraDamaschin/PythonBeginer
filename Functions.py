ec1608139bfb4550bc89ffb9eef952ae1f627821
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-29 19:03:09 +0100

    Return how many weeks and days mean an inputted int

--- Functions.py
+++ Functions.py
@@ -40,3 +40,16 @@ print("expected result: {}, actual result: {}".format(expected_result1, test1))
 test2 = population_density(864816, 121.4)
 expected_result2 = 7123.6902801
 print("expected result: {}, actual result: {}".format(expected_result2, test2))
+
+
+# 4. Return how many weeks and days mean an inputted int
+def readable_timedelta(days):
+    # use integer division to get the number of weeks
+    weeks = days // 7
+    # use % to get the number of days that remain
+    remainder = days % 7
+    return "{} week(s) and {} day(s).".format(weeks, remainder)
+
+
+# test your function
+print(readable_timedelta(10))
