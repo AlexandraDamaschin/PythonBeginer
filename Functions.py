5ccd993a5ab12174c4e991417d5d85f9dcf75850
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-29 18:55:08 +0100

    Population Density Function

--- Functions.py
+++ Functions.py
@@ -25,3 +25,18 @@ print('This function returned: {}'.format(return_value_1))
 print('\nCalling add_ten...')
 return_value_2 = add_ten(5)
 print('This function returned: {}'.format(return_value_2))
+
+
+# 3. Population Density Function
+def population_density(population, land_area):
+    return population / land_area
+
+
+# test cases for your function
+test1 = population_density(10, 1)
+expected_result1 = 10
+print("expected result: {}, actual result: {}".format(expected_result1, test1))
+
+test2 = population_density(864816, 121.4)
+expected_result2 = 7123.6902801
+print("expected result: {}, actual result: {}".format(expected_result2, test2))
