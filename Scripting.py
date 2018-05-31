b6ef9666a2f95436f47d2f4bf73b67fb9615b0cf
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-31 20:30:10 +0100

    input data

--- Scripting.py
+++ Scripting.py
@@ -1,3 +1,4 @@
+# 1. Snakes example
 how_many_snakes = 1
 snake_string = """
 Welcome to Python3!
@@ -12,4 +13,20 @@ Welcome to Python3!
 <3, Juno
 """
 
-print(snake_string *how_many_snakes)
+print(snake_string * how_many_snakes)
+
+# 2. Input string
+name = input('Please, Enter a name:')
+print('Hello', name.title())
+
+# 3. Input int
+number = int(input('Please, Enter a number'))
+print(number)
+
+# 4. Input int
+num = float(input('Please, Enter a number'))
+print(num)
+
+# 5.Input float transformed to int
+numb = int(float(input('Please, Enter a number')))
+print(numb)
