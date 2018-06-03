9293a4c1a6894d5b594c719ac0db197127ccbea3
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-06-03 16:16:37 +0100

    .strip()= remove newline character attached

--- Read_Write_File.py
+++ Read_Write_File.py
@@ -32,3 +32,11 @@ with open("camelot.txt") as song:
     print(song.read(2))
     print(song.read(8))
     print(song.read())
+
+# .strip()= remove newline character attached
+camelot_lines = []
+with open("camelot.txt") as f:
+    for line in f:
+        camelot_lines.append(line.strip())
+
+print(camelot_lines)
