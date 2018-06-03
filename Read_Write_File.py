a697bb1b6b8c870945b0d534d4992afbb42afc01
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-06-03 15:55:54 +0100

    Write to a file

--- Read_Write_File.py
+++ Read_Write_File.py
@@ -2,6 +2,14 @@
 # specify path and r-read only(by default)
 f = open('hello_file', 'r')
 file_data = f.read()
+# always close file because even not we will have Too Many Open Files
 f.close()
 
 print(file_data)
+
+# Write to a file
+# w- write, will delete everything from an existing file
+# if it`s a new file, he will create it
+f = open('hello_file2.txt', 'w')
+f.write("Hello there!")
+f.close()
